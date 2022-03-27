6da37bcc39b5be53071a3f65ad7c824fc12fb5d5
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-03-27 18:51:21 +1100

    functionality added to buttons

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const click = document.querySelector(".click");
+const btns = document.querySelectorAll(".btn");
 const counter = document.getElementById("counter");
 
 totalClicks = 0;
 
-click.addEventListener("click", () => {
-  totalClicks++;
-  counter.innerText = totalClicks;
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const btns = e.currentTarget.classList;
+    if (btns.contains("increase")) {
+      totalClicks++;
+    }
+    if (btns.contains("decrease")) {
+      totalClicks--;
+    }
+    counter.innerText = totalClicks;
+  });
 });
- 
